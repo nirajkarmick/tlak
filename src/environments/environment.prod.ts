17fb59a47a15295fa8cfeebe469d092dacaca88c
Niraj Gupta <niraj.karmick@gmail.com>
2022-06-07 17:03:25 +0530

    set variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
  // ip: "https://account.tlakapp.com/tlak/api/v7",
   pullit: "https://api.tutterflycrm.com/ptprog/api/",
   pullit_web: "https://api.tutterflycrm.com/ptprog/",
-  coordinateKey:'AIzaSyDe20rB3ZDxDKMs4NTje6ueQTTtp1JixVo',
+  coordinateKey:'',
 };
